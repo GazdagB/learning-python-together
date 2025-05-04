89c7f5e8313e62109f74f257427f1f1587ec34c3
GazdagB <gazdagbal@gmail.com>
2025-05-04 13:04:39 +0200

    ✨ Explore variables

---
+++ 03 Variables/types.py
@@ -0,0 +1,21 @@
+x = str(3)   # x will be '3'
+y = int(3)   # y will be 3
+z = float(3) # z will be 3.0
+print(x) # Outputs: 3
+print(y) # Outputs: 3
+print(z) # Outputs: 3.0
+
+print(type(x)) # Outputs: <class 'str'>
+print(type(y)) # Outputs: <class 'int'>
+print(type(z)) # Outputs: <class 'float'>
+
+# Single or double quotes are the same
+print("Hello")
+print('Hello')
+
+a = 4
+A = "BuggedOutCoder"
+# a and A are different variables
+
+print(a) # Outputs: 4
+print(A) # Outputs: 5 
