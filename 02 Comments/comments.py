35b7ca79533b96fa33c6ff73c319310d4c6e4ace
GazdagB <gazdagbal@gmail.com>
2025-05-04 12:34:59 +0200

    ✨ Exploring comments

---
+++ 02 Comments/comments.py
@@ -0,0 +1,16 @@
+# This is a comment 
+print("Hello, World!")
+
+# This is a comment too
+print("Hello, World!") # This is a comment too
+
+# This is a multiline comment
+# and it continues here
+# and here
+
+"""
+This is a string literal 
+but python treats it as a comment
+because it is not assigned to a variable
+"""
+
