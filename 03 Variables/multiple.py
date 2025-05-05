9174ab5cbafb6885d682585e24248dacf9e92537
GazdagB <gazdagbal@gmail.com>
2025-05-05 20:41:53 +0200

    ✨ Adds multiple variables

---
+++ 03 Variables/multiple.py
@@ -0,0 +1,18 @@
+# This code demonstrates how to assign multiple variables in Python.
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+# Assigning the same value to multiple variables
+a = b = c = "One"
+print(a)
+print(b)
+print(c)
+
+# Unpacking a collection or Destructuring assignment
+fruits = ["Two", "Three", "Four"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
