be2ef008cf9e39546511772c504d210a3e5843f5
GazdagB <gazdagbal@gmail.com>
2025-05-05 20:52:10 +0200

    ✨ Introduces print

---
+++ 03 Variables/output.py
@@ -0,0 +1,18 @@
+# x = "Python is awesome"
+# print(x)
+# Output: Python is awesome
+
+# Outputs the same as the code above but with multiple variables
+
+# a = "Python"
+# b = "is"
+# c = "awesome"
+# print(a,b,c)
+
+# This code demonstrates how to assign multiple variables in Python.
+x = 5
+y = 10
+print(x + y)  # Output: 15
+
+print("Hello World")  # Output: Hello World
+# This code demonstrates how to assign multiple variables in Python.
