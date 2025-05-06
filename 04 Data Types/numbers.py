d1951b964a4e055b0b72d827cbe1f21856c61a14
GazdagB <gazdagbal@gmail.com>
2025-05-06 21:10:52 +0200

    ✨ Adds numbers

---
+++ 04 Data Types/numbers.py
@@ -0,0 +1,18 @@
+import random 
+
+x = 1 # int 
+y = 2.5 # float
+z = 1j # complex
+
+print(type(x)) # <class 'int'>
+print(type(y)) # <class 'float'>
+print(type(z)) # <class 'complex'>
+
+a = 1.5e2 # scientific notation
+print(a) # 150.0
+
+b = 5j # complex number
+print(b) # 5j
+
+random = random.randint(1, 10) # random number between 1 and 10
+print(random) # random number between 1 and 10
