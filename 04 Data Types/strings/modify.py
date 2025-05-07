39a587d2a22fe91487b1d193619dbe8c02f059fa
GazdagB <gazdagbal@gmail.com>
2025-05-07 19:16:16 +0200

    ✨ Adds string examples

---
+++ 04 Data Types/strings/modify.py
@@ -0,0 +1,17 @@
+
+a = "Hello, World!"
+print(a.upper())
+print(a.lower())
+print(a.title())
+print(a.capitalize())
+print(a.swapcase())
+print(a.strip()) # removes leading and trailing spaces
+
+b = "Gold"
+print(b.replace("g", "b")) # replaces G with B
+
+c = "Witch"
+print(c.replace("i", "a")) # replaces W with B
+
+d = "Hello, World!"
+print(d.split(",")) # splits the string into a list of strings
