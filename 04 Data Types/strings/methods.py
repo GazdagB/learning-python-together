032f89db2fde600c5f516e2e081d263cd2ff5d3d
GazdagB <gazdagbal@gmail.com>
2025-05-07 20:56:16 +0200

    ✨ Adds methods

---
+++ 04 Data Types/strings/methods.py
@@ -0,0 +1,7 @@
+text = "lowercase"
+print(text.capitalize())  # Capitalizes the first character of the string
+print(text.upper())  # Converts the entire string to uppercase
+
+txt = "Hello, World!"
+print(txt.lower())  # Converts the entire string to lowercase
+print(txt.strip())  # Removes any whitespace from the beginning or the end of the string
