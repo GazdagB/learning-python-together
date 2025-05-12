4f9ffe5b39d8874a7ef45e96cf082103bee71c7f
GazdagB <gazdagbal@gmail.com>
2025-05-12 20:22:16 +0200

    ✨ Adds more boolean examples

--- 04 Data Types/boolean.py
+++ 04 Data Types/boolean.py
@@ -4,4 +4,16 @@ print(higher) # True
 print(bool("Hello"))
 print(bool(""))
 
-# TODO: Write another examples 
+
+print(bool(0)) # False
+print(bool(1)) # True
+print(bool(-1)) # True
+print(bool([])) # False
+print(bool([1, 2, 3])) # True
+print(bool({})) # False
+print(bool({"key": "value"})) # True
+print(bool(None)) # False
+print(bool(True)) # True
+print(bool(False)) # False
+print(bool(0.0)) # False
+print(bool(0.1)) # True
