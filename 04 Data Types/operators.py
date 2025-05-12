abb28e96845b575e1a587c6bf7267380b684abd0
GazdagB <gazdagbal@gmail.com>
2025-05-12 21:08:15 +0200

    ✨ Adds different operators

---
+++ 04 Data Types/operators.py
@@ -0,0 +1,24 @@
+
+# Arithmetic Operators
+# +, -, *, /, //, %, **
+
+# Addition
+print(5 + 10) 
+
+# Subtraction
+print(10 - 5)
+
+# Multiplication
+print(5 * 10)
+
+# Division
+print(10 / 5) # Returns a float
+
+# Floor Division
+print(10 // 3) # Returns an integer
+
+# Modulus
+print(10 % 3) # Returns the remainder
+
+# Exponentiation
+print(2 ** 3) # 2 raised to the power of 3
