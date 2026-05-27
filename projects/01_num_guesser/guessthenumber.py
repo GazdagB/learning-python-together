e933cb7083dd79226bd8236d79dd4b861b48cc29
Gazdag Balázs <gazdagbal@gmail.com>
2026-05-27 23:48:23 +0200

    ✨ improves code for project 1

--- projects/01_num_guesser/guessthenumber.py
+++ projects/01_num_guesser/guessthenumber.py
@@ -8,22 +8,22 @@ compsRandomNumb = random.randint(1, 100)
 
 while not isSolved:
 
-    x = input("Enter a number (0 - 100): ")
+    x = input('\x1b[0m' + "Enter a number (0 - 100): ")
     digit = 0
     if not  x.isdigit():
-        print("The input must be a number!")
+        print("\033[31m The input must be a number! \033[31m"  )
         continue
 
     digit = int(x)
 
     if digit <= 0 or digit > 100:
-        print("Number must be between 0 and 100!")
+        print("\033[31mNumber must be between 0 and 100!\033[31m")
         continue
 
     if digit == compsRandomNumb:
-        print("The number is correct! It was {}!".format(compsRandomNumb))
+        print("\033[32mThe number is correct! It was {}! \033[32m".format(compsRandomNumb))
         isSolved = True
     elif digit > compsRandomNumb:
-        print("Too high! Guess Downward!")
+        print("⬇️ Too high! Guess Downward!")
     elif digit < compsRandomNumb:
-        print("Too low! Guess Upper!")
+        print("⬆️ Too low! Guess Upper!")
